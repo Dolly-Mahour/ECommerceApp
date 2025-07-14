678597df408337fd56e02f5fd9c6523f93c32c14
Bajeshkumar <77668520+Bajeshkumar@users.noreply.github.com>
2025-07-14 12:29:03 +0530

    CI/CD pipeline working

---
+++ src/app/testing1/testing1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing1',
+  imports: [],
+  templateUrl: './testing1.component.html',
+  styleUrl: './testing1.component.css'
+})
+export class Testing1Component {
+
+}
---
+++ src/app/testing2/testing2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing2',
+  imports: [],
+  templateUrl: './testing2.component.html',
+  styleUrl: './testing2.component.css'
+})
+export class Testing2Component {
+
+}
