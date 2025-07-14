462bce8ee073d58945c21e01fbb986cf8a8f2803
Bajeshkumar <77668520+Bajeshkumar@users.noreply.github.com>
2025-07-14 13:04:23 +0530

    from branch 1 after adding component--

---
+++ src/app/fortesting3/fortesting3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fortesting3',
+  imports: [],
+  templateUrl: './fortesting3.component.html',
+  styleUrl: './fortesting3.component.css'
+})
+export class Fortesting3Component {
+
+}
