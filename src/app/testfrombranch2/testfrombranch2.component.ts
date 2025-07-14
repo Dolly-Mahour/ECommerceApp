cb10c6d8bafcde1f91f270f38ca0f66b95444abd
Bajeshkumar <77668520+Bajeshkumar@users.noreply.github.com>
2025-07-14 12:31:24 +0530

    commit from branch1

---
+++ src/app/testfrombranch2/testfrombranch2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testfrombranch2',
+  imports: [],
+  templateUrl: './testfrombranch2.component.html',
+  styleUrl: './testfrombranch2.component.css'
+})
+export class Testfrombranch2Component {
+
+}
