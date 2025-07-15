e035522f9304b80c8148ad49859fc011da0abb8b
Bajeshkumar <77668520+Bajeshkumar@users.noreply.github.com>
2025-07-15 16:38:09 +0530

    34

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ItemsListService } from './items-list.service';
+import { ItemsListAtCartService } from './items-list-at-cart.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes,withComponentInputBinding()),
+    ItemsListService,
+    ItemsListAtCartService,
     // provideRouter(appRoutes, withComponentInputBinding()),
   ]
 };
